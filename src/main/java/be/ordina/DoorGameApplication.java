707a2d4e657976aa9973e6c16feeaf28001639fc
andreasevers <andreas.evers@ordina.be>
2015-09-07 23:16:10 +0200

    Initial commit

---
+++ src/main/java/be/ordina/DoorGameApplication.java
@@ -0,0 +1,12 @@
+package be.ordina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoorGameApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DoorGameApplication.class, args);
+    }
+}
